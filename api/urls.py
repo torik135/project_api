71c745e774afda734c28d3e3c50edf68eac0477e
torik135 <riqibrekele@gmail.com>
2022-07-02 18:34:16 +0700

    add get project by slug

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import getProject, getTech
+from .views import getProject, getTech, getProjectBySlug
 
 urlpatterns = [
+  # PROJECTS
   path('projects/all/', getProject),
+  path('projects/<slug:slug>/', getProjectBySlug),
+  # path('projects/<int:id>/', getProjectById),
+  # TECH
   path('tech/all/', getTech),
+  # path('tech/<str:slug>/', getTechBySlug),
 ]
--- api/views.py
+++ api/views.py
@@ -11,7 +11,24 @@ def getProject(req):
   return Response(project_serializers.data)
 
 @api_view(['GET'])
+def getProjectBySlug(req, slug):
+  project_slug_query = ProjectList.objects.get(slug=slug)
+  project_slug_serializers = ProjectListSerializers(project_slug_query, many=False)
+  return Response(project_slug_serializers.data)
+
+
+# Query err <----ERROR
+# @api_view(['GET'])
+# def getProjectById(req, id):
+#   project_id_query = ProjectList.objects.filter(pk=id).get()
+#   project_id_serializers = ProjectListSerializers(project_id_query, many=False)
+#   return Response(project_id_serializers.data)
+
+@api_view(['GET'])
 def getTech(req):
   tech_query = TechChoices.objects.all()
   tech_serializers = TechChoicesSerializers(tech_query, many=True)
   return Response(tech_serializers.data)
+
+# def getTechBySlug(req, project_id):
+
