ba28a8ca6df1837286db9cc2ba88500f26111882
torik135 <riqibrekele@gmail.com>
2022-07-02 23:13:05 +0700

    add get project by project type

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
   getProject, 
   getProjectBySlug,
+  getProjectByProjectType,
   getTech,
   getTechBySlug,
   )
@@ -10,6 +11,7 @@ urlpatterns = [
   # PROJECTS
   path('projects/all/', getProject),
   path('projects/<slug:slug>/', getProjectBySlug),
+  path('projects/type/<str:project_type>/', getProjectByProjectType),
   # TECH
   path('tech/all/', getTech),
   path('tech/<slug:tech_slug>/', getTechBySlug),
--- api/views.py
+++ api/views.py
@@ -5,25 +5,31 @@ from app_portofolio.models import ProjectList, TechChoices
 from .serializers import ProjectListSerializers, TechChoicesSerializers
 
 @api_view(['GET'])
-def getProject(req):
+def getProject(request):
   project_query = ProjectList.objects.all()
   project_serializers = ProjectListSerializers(project_query, many=True)
   return Response(project_serializers.data)
 
 @api_view(['GET'])
-def getProjectBySlug(req, slug):
+def getProjectBySlug(request, slug):
   project_slug_query = ProjectList.objects.get(slug=slug)
   project_slug_serializers = ProjectListSerializers(project_slug_query, many=False)
   return Response(project_slug_serializers.data)
 
 @api_view(['GET'])
-def getTech(req):
+def getProjectByProjectType(request, project_type):
+  project_pt_query = ProjectList.objects.filter(project_type=project_type)
+  project_pt_serializers = ProjectListSerializers(project_pt_query, many=True)
+  return Response(project_pt_serializers.data)
+
+@api_view(['GET'])
+def getTech(request):
   tech_query = TechChoices.objects.all()
   tech_serializers = TechChoicesSerializers(tech_query, many=True)
   return Response(tech_serializers.data)
 
 @api_view(['GET'])
-def getTechBySlug(req, tech_slug):
+def getTechBySlug(request, tech_slug):
   tech_slug_query = TechChoices.objects.get(tech_slug=tech_slug)
   tech_slug_serializers = TechChoicesSerializers(tech_slug_query, many=False)
   return Response(tech_slug_serializers.data)
